d5180f14765177f07f9ba6f27c32bde4f743fcac
gagimv <hagimumb123@outlook.jp>
2026-05-07 00:13:05 +0800

    Simplify: Minimal MainActivity for compilation
    - Remove complex UI files with compilation errors
    - Simple Hello World MainActivity
    - Use correct package name com.hma.rust

--- android/app/src/main/java/com/hma/MainActivity.kt
+++ android/app/src/main/java/com/hma/MainActivity.kt
@@ -1,30 +1,26 @@
-package com.hma
+package com.hma.rust
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.hma.ui.AppListScreen
-import com.hma.ui.MainScreen
-import com.hma.viewmodel.MainViewModel
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HMATheme {
+            MaterialTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HMAApp()
+                    Greeting("Hide My Applist")
                 }
             }
         }
@@ -32,89 +28,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HMAApp() {
-    val navController = rememberNavController()
-    val viewModel: MainViewModel = viewModel()
-    val hookStatus by viewModel.hookStatus.collectAsState()
-    val apps by viewModel.apps.collectAsState()
-    val config by viewModel.config.collectAsState()
-    val logs by viewModel.logs.collectAsState()
-    var showAboutDialog by remember { mutableStateOf(false) }
-    
-    NavHost(navController, startDestination = "main") {
-        composable("main") {
-            MainScreen(
-                hookStatus = hookStatus,
-                onToggleHook = { viewModel.toggleHook() },
-                onNavigateToApps = { navController.navigate("apps") },
-                onNavigateToConfig = { navController.navigate("config") },
-                onNavigateToTemplates = { navController.navigate("templates") },
-                onNavigateToLogs = { navController.navigate("logs") },
-                onNavigateToSettings = { navController.navigate("settings") }
-            )
-        }
-        
-        composable("apps") {
-            AppListScreen(
-                apps = apps,
-                onAppToggle = { viewModel.toggleApp(it) },
-                onBack = { navController.popBackStack() }
-            )
-        }
-        
-        composable("config") {
-            ConfigScreen(
-                scopes = config.scope,
-                onAddScope = { viewModel.addScope(it) },
-                onRemoveScope = { viewModel.removeScope(it) },
-                onEditScope = { pkg, cfg -> viewModel.editScope(pkg, cfg) },
-                onBack = { navController.popBackStack() }
-            )
-        }
-        
-        composable("templates") {
-            TemplateScreen(
-                templates = config.templates,
-                onAddTemplate = { name, apps -> viewModel.addTemplate(name, apps) },
-                onRemoveTemplate = { viewModel.removeTemplate(it) },
-                onEditTemplate = { name, tpl -> viewModel.editTemplate(name, tpl) },
-                onBack = { navController.popBackStack() }
-            )
-        }
-        
-        composable("logs") {
-            LogScreen(
-                logs = logs,
-                onClearLogs = { viewModel.clearLogs() },
-                onRefresh = { viewModel.refreshLogs() },
-                onBack = { navController.popBackStack() }
-            )
-        }
-        
-        composable("settings") {
-            SettingsScreen(
-                detailLog = config.detailLog,
-                maxLogSize = config.maxLogSize,
-                onDetailLogChange = { viewModel.setDetailLog(it) },
-                onMaxLogSizeChange = { viewModel.setMaxLogSize(it) },
-                onExportConfig = { viewModel.exportConfig() },
-                onImportConfig = { viewModel.importConfig() },
-                onCheckUpdate = { viewModel.checkUpdate() },
-                onAbout = { showAboutDialog = true },
-                onBack = { navController.popBackStack() }
-            )
-        }
-    }
-    
-    if (showAboutDialog) {
-        AboutDialog(onDismiss = { showAboutDialog = false })
-    }
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
+@Preview(showBackground = true)
 @Composable
-fun HMATheme(content: @Composable () -> Unit) {
-    MaterialTheme(
-        colorScheme = darkColorScheme(),
-        content = content
-    )
+fun GreetingPreview() {
+    MaterialTheme {
+        Greeting("Hide My Applist")
+    }
 }
